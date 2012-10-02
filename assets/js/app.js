a4fc1bd896a273e38601d9f570fb5a6b644b379f
Derek McBurney <d.mcburney@gmail.com>
2012-10-01 22:05:30 -0600

    Made server-side data-validation real time.
    Best of both worlds. Server-side validation for security, but it now
    updates upon real-time editing.

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,27 @@ $(function() {
 	
 	$('input, textarea').placeholder();
 	
+	$('#name, #email, #story').bind('input', function(event) { 
+		var target = event.target.id;
+		var data = {};
+		data[target] = $('#' + target).val();
+		$.ajax({
+			type: 'POST',
+			url: "assets/php/emailer.php",
+			data: data,
+			dataType: 'json',
+			success: function(response) {
+				if (response[target]) {
+					$("#" + target).addClass("error");
+					$("#" + target + "_error").html(response[target]).css('display', 'block');
+				} else if (!response[target]) {
+					$("#" + target + "_error").css('display', 'none');
+					$("#" + target).removeClass("error");
+				}
+			}
+		});
+	});
+	
 	$("#contactme").submit(function() {
 		//$("#load").show();
 		$('#send').hide();
