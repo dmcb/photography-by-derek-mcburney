6c944a3b730777f40daca1aa7a7da687a33105da
Derek McBurney <d.mcburney@gmail.com>
2013-11-04 09:17:42 -0700

    Adding XML sitemap for Drupal blog.
    This way Drupal pages will be crawled properly.

--- blog/sites/all/modules/embed_image/templates/embed_image.tpl.php
+++ blog/sites/all/modules/embed_image/templates/embed_image.tpl.php
@@ -5,4 +5,4 @@
  * Template file for embed image.
  */
 ?>
-<img src="<?php print $uri;?>" alt="<?php print $alt;?>" title="<?php print $title;?>" class="picture" />
+<div class="image-container"><img src="<?php print file_create_url( drupal_get_path('module', 'lazyloader')); ?>/image_placeholder.gif" data-src="<?php print $uri;?>" alt="<?php print $alt;?>" title="<?php print $title;?>" class="picture" /></div>
--- blog/sites/all/modules/embed_image/templates/embed_pinterest.tpl.php
+++ blog/sites/all/modules/embed_image/templates/embed_pinterest.tpl.php
@@ -5,6 +5,6 @@
  * Template file for embed pinterest.
  */
 ?>
-<a href="//www.pinterest.com/pin/create/button/?url=<?php print $uri; ?>&description=<?php print $title; ?>" data-pin-do="buttonPin" data-pin-config="none" target="_blank">
+<a href="//www.pinterest.com/pin/create/button/?url=<?php print $path; ?>&media=<?php print $uri; ?>&description=<?php print $title; ?>" data-pin-do="buttonPin" data-pin-config="none" target="_blank">
 	<img src="<?php print file_create_url( drupal_get_path('module', 'embed_image')); ?>/images/pin_it_button.png" class="pinit">
 </a>
