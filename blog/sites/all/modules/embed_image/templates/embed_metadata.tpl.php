03ee865d3e3640494b0d3df0c7b7ab98984c171a
Derek McBurney <d.mcburney@gmail.com>
2013-11-03 18:15:53 -0700

    Tweaks to image and metadata appearance.
    Also put the background off-white.

--- blog/sites/all/modules/embed_image/templates/embed_metadata.tpl.php
+++ blog/sites/all/modules/embed_image/templates/embed_metadata.tpl.php
@@ -4,11 +4,18 @@
  * @file
  * Template file for embed image metadata.
  */
-?>
-<ul class="metadata">
-<?php
+
+if (!empty($title)) {
+	print '<p class="title">'.$title.'</p>';
+}
+
+if (!empty($image_meta)) {
+	print '<ul class="metadata">';
+
 	foreach ($image_meta as $class => $value) {
 		print '<li class="'.$class.'">'.$value.'</li>';
 	}
+
+	print '</ul>';
+}
 ?>
-</ul>
