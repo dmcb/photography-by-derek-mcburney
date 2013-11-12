1b63e05f6773bdd448ad11cb97ed0cef624c8e0a
Derek McBurney <d.mcburney@gmail.com>
2013-11-11 18:16:55 -0700

    Add viewport header.
    The blog will now appear the way it should in mobile.

--- blog/sites/all/themes/photographybyderek_blog/template.php
+++ blog/sites/all/themes/photographybyderek_blog/template.php
@@ -1,5 +1,16 @@
 <?php
 
+function photographybyderek_blog_preprocess_html(&$variables) {
+	$viewport = array(
+		'#tag' => 'meta', 
+		'#attributes' => array(
+			'name' => 'viewport', 
+			'content' => 'width=device-width, initial-scale=1',
+		),
+	);
+	drupal_add_html_head($viewport, 'viewport');
+}
+
 function photographybyderek_blog_preprocess_node(&$variables) {
 	/* Remove read more link */
 	unset($variables['content']['links']['node']['#links']['node-readmore']);
