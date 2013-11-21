cf0e33e394e6108157643459b9182f5d75171d53
Derek McBurney <d.mcburney@gmail.com>
2013-11-20 21:26:43 -0700

    Adding default site settings file.
    This should have been in Git a while ago. It could have been deduced
    from the code, but why put someone through that nonsense!?

---
+++ assets/php/site_settings.default.php
@@ -0,0 +1,11 @@
+<?php
+# Copy site_settings.default.php to site_settings.php with fields filled out for deployment
+
+# Base URL of site
+$base_url = "";
+
+# Facebook album ids
+$categories = array(
+	'weddings-engagements' => '',
+	'people-events' => '',
+	'landscapes-architecture' => '');
