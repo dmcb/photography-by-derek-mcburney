83b0ba48db153c1ba7e175fa180300276acfe38a
Derek McBurney <d.mcburney@gmail.com>
2013-11-20 21:46:00 -0700

    Creating a cache thoroughly if none exists.
    And supressing warning messages that may interfere with Backbone.js.

--- assets/php/photos_to_json.php
+++ assets/php/photos_to_json.php
@@ -4,9 +4,14 @@
 require_once("facebook_photo_grab.php");
 require_once("site_settings.php");
 
+// Create cache if none exists
+if (!file_exists("cache")) {
+	@mkdir("cache");
+}
+
 // Grab facebook information if no cache exists or the cache is stale
 $cache_file = "cache/cache.json";
-$json = file_get_contents($cache_file);
+$json = @file_get_contents($cache_file);
 if ($json === false || time() - filemtime($cache_file) > 86400) {
 	$json = json_encode(process_photos($categories));
 	file_put_contents($cache_file, $json);
