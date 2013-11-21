563ae441c2dd9ee7140695892058ba0f7caecdce
Derek McBurney <d.mcburney@gmail.com>
2013-11-20 21:40:22 -0700

    Caching Facebook results.
    Loading photos from Facebook was noticably slow. Results will be cached
    for a day (too bad there’s no way I can see updating Facebook sending a
    cache clear).

--- assets/php/photos_to_json.php
+++ assets/php/photos_to_json.php
@@ -4,5 +4,11 @@
 require_once("facebook_photo_grab.php");
 require_once("site_settings.php");
 
-// Dump JSON
-print json_encode(process_photos($categories));
+// Grab facebook information if no cache exists or the cache is stale
+$cache_file = "cache/cache.json";
+$json = file_get_contents($cache_file);
+if ($json === false || time() - filemtime($cache_file) > 86400) {
+	$json = json_encode(process_photos($categories));
+	file_put_contents($cache_file, $json);
+}
+print $json;
