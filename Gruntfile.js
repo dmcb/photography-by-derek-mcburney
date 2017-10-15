ef558260745831432c539242396626d663cfedae
Derek McBurney <d.mcburney@icloud.com>
2017-10-15 01:43:51 -0600

    Adds webp output and changes JPEG compression

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,7 @@
 module.exports = function(grunt) {
+  const webp = require('imagemin-webp');
+  const mozjpeg = require('imagemin-mozjpeg');
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
@@ -7,37 +10,31 @@ module.exports = function(grunt) {
         options: {
           sizes: [{
             suffix: '.mobile',
-            quality: 90,
             rename: false,
             width: 356,
             height: 427
           },{
             suffix: '.mobile.2x',
-            quality: 85,
             rename: false,
             width: 712,
             height: 854
           },{
             suffix: '.tablet',
-            quality: 90,
             rename: false,
             width: 610,
             height: 407
           },{
             suffix: '.tablet.2x',
-            quality: 85,
             rename: false,
             width: 1220,
             height: 813
           },{
             suffix: '.desktop',
-            quality: 90,
             rename: false,
             width: 1250,
             height: 833,
           },{
             suffix: '.desktop.2x',
-            quality: 85,
             rename: false,
             width: 2500,
             height: 1667
@@ -47,20 +44,18 @@ module.exports = function(grunt) {
           expand: true,
           src: ['*.jpg'],
           cwd: 'src/photos/blog/',
-          dest: '_site/photos/blog/'
+          dest: 'resized-photos/blog/'
         }]
       },
       blogIndex: {
         options: {
           sizes: [{
             suffix: '.mobile',
-            quality: 90,
             rename: false,
             width: 300,
             height: 300
           },{
             suffix: '.mobile.2x',
-            quality: 85,
             rename: false,
             width: 600,
             height: 600
@@ -70,68 +65,58 @@ module.exports = function(grunt) {
           expand: true,
           src: ['*.jpg'],
           cwd: 'src/photos/blog-index/',
-          dest: '_site/photos/blog-index/'
+          dest: 'resized-photos/blog-index/'
         }]
       },
       portfolio: {
         options: {
           sizes: [{
             suffix: '.mobile',
-            quality: 90,
             rename: false,
             width: 365,
             height: 243
           },{
             suffix: '.mobile.2x',
-            quality: 85,
             rename: false,
             width: 730,
             height: 486
           },{
             suffix: '.tablet',
-            quality: 90,
             rename: false,
             width: 719,
             height: 479
           },{
             suffix: '.tablet.2x',
-            quality: 85,
             rename: false,
             width: 1438,
             height: 958
           },{
             suffix: '.desktop',
-            quality: 90,
             rename: false,
             width: 946,
-            height: 631,
+            height: 631
           },{
             suffix: '.desktop.2x',
-            quality: 85,
             rename: false,
             width: 1892,
             height: 1262
           },{
             suffix: '.desktop-medium',
-            quality: 90,
             rename: false,
             width: 1310,
-            height: 873,
+            height: 873
           },{
             suffix: '.desktop-medium.2x',
-            quality: 85,
             rename: false,
             width: 2620,
             height: 1746
           },{
             suffix: '.desktop-large',
-            quality: 90,
             rename: false,
             width: 1758,
-            height: 1172,
+            height: 1172
           },{
             suffix: '.desktop-large.2x',
-            quality: 85,
             rename: false,
             width: 3516,
             height: 2344
@@ -141,14 +126,13 @@ module.exports = function(grunt) {
           expand: true,
           src: ['*.jpg'],
           cwd: 'src/photos/portfolio/',
-          dest: '_site/photos/portfolio/'
+          dest: 'resized-photos/portfolio/'
         }]
       },
       tiny: {
         options: {
           sizes: [{
             suffix: '.tiny',
-            quality: 90,
             rename: false,
             width: 36,
             height: 36
@@ -158,7 +142,7 @@ module.exports = function(grunt) {
           expand: true,
           src: ['**/*.jpg'],
           cwd: 'src/photos/',
-          dest: 'src/photos-tiny/'
+          dest: 'resized-photos-tiny/'
         }]
       },
     },
@@ -166,19 +150,39 @@ module.exports = function(grunt) {
     imagemin: {
       photos: {
         options: {
-          optimizationLevel: 4
+          use: [mozjpeg()],
+          progressive: true,
+          quality: 80
         },
         files: [{
           expand: true,
-          cwd: '_site/photos/',
+          cwd: 'resized-photos/',
           src: ['**/*.jpg'],
           dest: '_site/photos/'
         }]
       },
-      photosTiny: {
+      photosWebP: {
+        options: {
+          use: [webp()],
+          quality: 50
+        },
         files: [{
           expand: true,
-          cwd: 'src/photos-tiny/',
+          cwd: 'resized-photos/',
+          src: ['**/*.jpg'],
+          dest: '_site/photos/',
+          ext: '.webp',
+          extDot: 'last'
+        }]
+      },
+      photosTiny: {
+        options: {
+          use: [mozjpeg()],
+          quality: 50
+        },
+        files: [{
+          expand: true,
+          cwd: 'resized-photos-tiny/',
           src: ['**/*.jpg'],
           dest: 'src/photos-tiny/'
         }]
